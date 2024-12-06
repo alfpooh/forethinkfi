854becd42cbb0baa11017616b3f95c6dd12d0541
Alf Bae <alfpooh@gmail.com>
2024-12-06 14:46:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const icons = document.querySelectorAll("[data-lucide]");
     lucide.createIcons();
 });
